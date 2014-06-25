c9da9fe67f5e81b7ebc95f3667c17b1861a48174
James Stewart <jstewart101@gmail.com>
2014-06-26 08:16:22 +1000

    Version bump for changes to component names.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='braubuddy',
-    version='0.1.0',
+    version='0.2.0',
     author='James Stewart',
     author_email='jstewart101@gmail.com',
     packages=['braubuddy'],
