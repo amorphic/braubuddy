770b4e4bb8226d56f10a1910bcd4b6e42d6b6e38
James Stewart <jstewart101@gmail.com>
2014-07-17 18:03:12 +1000

    Add jsonfile unit test.

---
+++ braubuddy/tests/outputs/test_jsonfile.py
@@ -0,0 +1,56 @@
+"""
+Braubuddy JSONFileOutput unit tests
+"""
+
+import unittest
+import json
+from random import randrange 
+from time import time
+from tempfile import NamedTemporaryFile
+from braubuddy.output import jsonfile
+
+
+class JSONFileOutput(unittest.TestCase):
+
+    def test_publish_metric(self):
+        """A single metric is published."""
+        outfile = NamedTemporaryFile()
+        output = jsonfile.JSONFileOutput(
+            units='fahrenheit',
+            out_file=outfile.name
+        )
+        t = int(time())
+        output.publish_status(25, 20, 100, 0)
+        self.assertEquals(
+            json.load(outfile)[0],
+            [25, 20, 100, 0, t]
+        )
+
+    def test_publish_metrics(self):
+        """Multiple metrics are published."""
+        outfile = NamedTemporaryFile()
+        output = jsonfile.JSONFileOutput(
+            units='fahrenheit',
+            out_file=outfile.name
+        )
+        expected = []
+        # publish metrics
+        for d in range(20):
+            target = 20
+            temp = randrange(15,25)
+            if temp > target:
+                heat = 0
+                cool = 100
+            elif temp < target:
+                heat = 100
+                cool = 0
+            else:
+                heat=0
+                cool=0
+            now = int(time())
+            output.publish_status(target, temp, heat, cool)
+            expected.append([target, temp, heat, cool, now])
+        self.assertEquals(
+            json.load(outfile),
+            expected
+        )
