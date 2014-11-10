2a3bd2727c225e98ca435f856e6d23d104b1c6ae
amorphic <jstewart101@gmail.com>
2014-11-11 08:55:32 +1100

    Release 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='braubuddy',
-    version='0.4.0',
+    version='0.4.1',
     author='James Stewart',
     author_email='jstewart101@gmail.com',
     description='An extensile temperature management framework.',
