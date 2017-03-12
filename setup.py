9bafc15144dc45f2765d55850a92bd6e911f1cce
James Stewart <jstewart101@gmail.com>
2017-03-12 23:56:42 +1100

    Release 0.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='braubuddy',
-    version='0.4.2',
+    version='0.4.3',
     author='James Stewart',
     author_email='jstewart101@gmail.com',
     description='An extensile temperature management framework.',
